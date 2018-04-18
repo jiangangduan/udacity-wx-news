f840e9bede5208cb5a3d119ac6881ab0e5be6754
jiangang <jiangangduan@gmail.com>
2018-04-18 11:56:33 +0800

    get list almost done

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,42 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+    newstype: 'gn',
+    newslist: []
+  },  
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    this.getNewsList(this.data.newstype)
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  getNewsList(newstype) {
+    console.log("news type is "+newstype)
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: newstype
+      },
+      success: res => {        
+        let result = res.data.result
+        let newslist = []        
+        for (let i = 0; i < result.length; i++) {          
+          let theDate = new Date(result[i].date)             
+          let theSource = "未知新闻来源"
+          if (result[i].source == null) {
+            let theSource = result[i].source
+          } 
+
+          newslist.push({
+            id: result[i].id,
+            title: result[i].title,
+            source: theSource,
+            time: theDate.getHours() + ":" + theDate.getMinutes(),
+            image: result[i].firstImage       
+            
+          })
+        }        
+        this.setData({
+          newslist: newslist
+        })   
+      }
     })
   }
 })
